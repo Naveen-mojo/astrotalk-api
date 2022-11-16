c740785f005337bbab3d6c376c3b2bb142688ac4
Naveen-mojo <naveenmojo123@gmail.com>
2022-11-16 12:50:43 +0530

    config file changes

--- server.js
+++ server.js
@@ -81,7 +81,6 @@ io.of('/stream').on('connection', stream);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-  const port = server.address().port;
   console.log(`Server is running on port ${PORT}.`);
 });
 
