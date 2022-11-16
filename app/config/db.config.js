74336b2f269844cee87d66b0daf9a6138338590e
Naveen-mojo <naveenmojo123@gmail.com>
2022-11-16 16:55:09 +0530

    config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -5,8 +5,8 @@
 // };
 
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://naveen:naveenojha@cluster0.4zqsizj.mongodb.net/astrotalk?retryWrites=true&w=majority', {
+// mongodb+srv://naveen:naveenojha@cluster0.4zqsizj.mongodb.net/astrotalk?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://cluster0.4zqsizj.mongodb.net/astrotalk', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
