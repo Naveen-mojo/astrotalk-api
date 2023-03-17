1adcefa21228647a263e13816bdc5f5f250d6fbc
Naveen-mojo <naveenmojo123@gmail.com>
2023-03-17 19:11:14 +0530

    conversation

--- app/controllers/conversation.controller.js
+++ app/controllers/conversation.controller.js
@@ -3,15 +3,49 @@ const { conversation: Conversation } = db;
 
 // Add Conversation
 exports.addConversation = async (req, res) => {
-    if (!req.body.senderId || !req.body.receiverId) {
-        res.status(400).send({ message: "please fill all required fields" })
-        return;
-    }
+    // if (!req.body.senderId || !req.body.receiverId) {
+    //     res.status(400).send({ message: "please fill all required fields" })
+    //     return;
+    // }
+    // const newConversation = new Conversation({
+    //     members: [req.body.receiverId, req.body.senderId],
+    // });
+
     const newConversation = new Conversation({
-        members: [req.body.senderId, req.body.receiverId],
-    });
+        "astroId": req.body.astroId,
+        "userId": req.body.userId,
+        "message": {
+            "text": req.body.textMesg,
+            "sender": req.body.sender,
+            "chattime": new Date()
+        }
+    })
 
     try {
+
+        const conversationFind = await Conversation.findOne({
+            astroId: req.body.astroId, userId: req.body.userId,
+        });
+
+        console.log("conversationFind", conversationFind);
+
+        if (conversationFind) {
+            const data = await Conversation.message.updateMany({ _id: conversationFind._id }, {
+                $set: [{
+                    text: 'Helllo',
+                    sender: 'astro',
+                    chattime: new Date()
+                },
+                {
+                    text: 'Helllo',
+                    sender: 'astro',
+                    chattime: new Date()
+                }]
+            }, {
+                new: true
+            })
+            console.log("data", data);   
+        }
         const savedConversation = await newConversation.save();
         res.status(200).json(savedConversation);
     } catch (err) {
